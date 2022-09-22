2c837e2856f4fa0ec70cb8858dd7e832cb43aae4
AnuR01 <raw.anu@outlook.com>
2022-09-23 01:29:32 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { useState } from 'react';
 import './App.css';
-import {useState,useEffect } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 
-const URL = 'https://api.exchangeratesapi.io/v1/latest? access_key='
+
+const URL = 'https://api.exchangeratesapi.io/v1/latest? access_key=';
 const API_KEY = '911b3806a1cbe40dacf96b52c007b3f3';
 
 function App() {
@@ -19,11 +19,11 @@ const  [rate, setRate] = useState(0);
           <label> Eur </label>&nbsp;
           <input type="number" step="0.01"
           value={eur} onChange={e => setEur(e.target.value)} />
-          <output> {rate}</output>
+          <output>{rate}</output>
         </div>
 
         <div>
-          <label> Gbp </label>&nbsp;
+          <label>Gbp</label>
           <output> {gbp.toFixed(2)} €</output>
         </div>
 
@@ -38,15 +38,24 @@ const  [rate, setRate] = useState(0);
   
 }
 
-async functionconvert(e) {
+async function convert(e) {
   e.preventDefaut();
   try {
     const address = URL + API_KEY;
     const response = await fetch(address);
 
     if (response.ok) {
-      const json = await response.json()
+      const json = await response.json();
+      console.log(json.rate.GBP);
+      setRate(json.rates.GBP);
+
+      setGbp(eur * json,rates.GBP);
+    } else {
+      alert('Error retrieving exchange rate.');
+      console.log(response);
     }
+  } catch (err) {
+    alert(err);
   }
 }
 
