ac9bb506319721b79b688da0f6658461df64e618
AnuR01 <raw.anu@outlook.com>
2022-09-23 18:53:35 +0530

    fixed some errors in code

--- src/App.js
+++ src/App.js
@@ -3,20 +3,41 @@ import { useState } from 'react';
 import axios from 'axios';
 
 
-const URL = 'https://api.exchangerate.host/latest';
-const API_KEY = '911b3806a1cbe40dacf96b52c007b3f3';
+const URL = 'https://api.exchangerate.host/latest'
+const API_KEY = '911b3806a1cbe40dacf96b52c007b3f3'
 
 function App() {
 const  [eur, setEur] = useState(0);
 const  [gbp, setGbp] = useState(0);
 const  [rate, setRate] = useState(0);
 
+async function convert(e) {
+  e.preventDefaut();
+  try {
+    const address = URL + API_KEY;
+    const response = await fetch(address);
+
+    if (response.ok) {
+      const json = await response.json();
+      console.log(json.rates.GBP);
+      setRate(json.rates.GBP);
+
+      setGbp(eur * json.rates.GBP);
+    } else {
+      alert('Error retrieving exchange rate.');
+      console.log(response);
+    }
+  } catch (err) {
+    alert(err);
+  }
+}
+
 
   return (
     <div id="container">
       <form onSubmit={convert}>
       <div>
-          <label> Eur </label>&nbsp;
+          <label>Eur</label>&nbsp;
           <input type="number" step="0.01"
           value={eur} onChange={e => setEur(e.target.value)} />
           <output>{rate}</output>
@@ -24,7 +45,7 @@ const  [rate, setRate] = useState(0);
 
         <div>
           <label>Gbp</label>
-          <output> {gbp.toFixed(2)} €</output>
+          <output>{gbp.toFixed(2)} € </output>
         </div>
 
         <div>
@@ -38,25 +59,6 @@ const  [rate, setRate] = useState(0);
   
 }
 
-async function convert(e) {
-  e.preventDefaut();
-  try {
-    const address = URL + API_KEY;
-    const response = await fetch(address);
-
-    if (response.ok) {
-      const json = await response.json();
-      console.log(json.rate.GBP);
-      setRate(json.rates.GBP);
-
-      setGbp(eur * json,rates.GBP);
-    } else {
-      alert('Error retrieving exchange rate.');
-      console.log(response);
-    }
-  } catch (err) {
-    alert(err);
-  }
-}
 
 export default App;
+
